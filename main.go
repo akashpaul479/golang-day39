dcf086e80516f9f6e763c0f6ac3e14c6dbcd9a18
akashpaul479 <akashpaul4790@gmail.com>
2026-01-27 20:11:04 +0530

    error handling

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "revision/revision"
+
+func main() {
+	revision.ErrorHandling()
+}
---
+++ revision/errorhandling.go
@@ -0,0 +1,147 @@
+package revision
+
+import (
+	"errors"
+	"fmt"
+)
+
+type User struct {
+	Id    int
+	Name  string
+	Email string
+}
+
+var users = make(map[int]User)
+var nextID = 1
+
+// create error handling
+func CreateUser(name, email string) error {
+	if name == "" || email == "" {
+		return errors.New("name and email cannot be empty")
+	}
+	for _, u := range users {
+		if u.Email == email {
+			return errors.New("email already exsists")
+		}
+	}
+	user := User{
+		Id:    nextID,
+		Name:  name,
+		Email: email,
+	}
+	users[nextID] = user
+	nextID++
+
+	return nil
+}
+
+// read error handling
+func ReadUser(id int) (User, error) {
+	if id <= 0 {
+		return User{}, errors.New("invalid user id")
+	}
+	user, exsists := users[id]
+	if !exsists {
+		return User{}, errors.New("user not found ")
+	}
+	return user, nil
+}
+
+// update error handling
+func Updateuser(id int, name string) error {
+	if id <= 0 {
+		return errors.New("invalid id")
+	}
+	if name == "" {
+		return errors.New("name cannot be empty")
+	}
+	user, exsists := users[id]
+	if !exsists {
+		return errors.New("user not found")
+	}
+	user.Name = name
+	users[id] = user
+	return nil
+}
+
+// Delete error handling
+func DeleteUser(id int) error {
+	if id <= 0 {
+		return errors.New("invalid id")
+	}
+	if _, exists := users[id]; !exists {
+		return errors.New("user not found")
+	}
+	delete(users, id)
+
+	return nil
+}
+func ErrorHandling() {
+	for {
+		fmt.Println("-----CRUD operations-----")
+		fmt.Println("1.Create user")
+		fmt.Println("2.Get user")
+		fmt.Println("3.Update user")
+		fmt.Println("4. Delete user")
+		fmt.Println("5. Exit")
+		fmt.Print("Choose option: ")
+
+		var choice int
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			var name, email string
+			fmt.Println("Enter name:")
+			fmt.Scan(&name)
+			fmt.Println("Enter email:")
+			fmt.Scan(&email)
+
+			if err := CreateUser(name, email); err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Println("user created succesfully")
+			}
+		case 2:
+			var id int
+			fmt.Println("Enter id:")
+			fmt.Scan(&id)
+
+			user, err := ReadUser(id)
+			if err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Printf("ID: %d, name: %s, Email:%s\n", user.Id, user.Name, user.Email)
+			}
+		case 3:
+			var id int
+			var name string
+			fmt.Println("Enter user id:")
+			fmt.Scan(&id)
+			fmt.Println("Enter new name:")
+			fmt.Scan(&name)
+
+			if err := Updateuser(id, name); err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Println("Updated succesfully")
+			}
+		case 4:
+			var id int
+			fmt.Println("Enter id:")
+			fmt.Scan(&id)
+			if err := DeleteUser(id); err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Println("deleted succesfully")
+			}
+		case 5:
+			fmt.Println("Exiting succesfully...")
+
+			return
+
+		default:
+			fmt.Println("Invalid choice")
+		}
+	}
+}
